1679fca0457d36a19c354f6c4e744085fd9d07b0
OnlyPaul <pavares.cha@gmail.com>
2019-10-21 11:52:43 +0200

    Commit initial structure

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='pysliceplorer',
+    version='0.1',
+    description='Python Library for Slicing Visualization',
+    url='https://github.com/OnlyPaul/pysliceplorer',
+    author='Pavares Charoenchaipiyakul',
+    author_email='pavares.cha@gmail.com',
+    license='MIT',
+    packages=['pysliceplorer'],
+    zip_safe=False
+)
